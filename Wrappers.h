55934ee0276f8576a2e54bb8c0f7576c8bd75c0c
rlee42 <roc.lee@gmail.com>
2020-03-17 04:34:54 +0000

    added wrappers.cpp

--- Wrappers.h
+++ Wrappers.h
@@ -1,88 +1,70 @@
+#include "BoulderProblem.h" //need to include because of nested class
+#include "TopRopeRoute.h"
+struct Mountain;
+struct Shoe;
+struct TripPlan;
+struct ClimbPlan;
 
 struct BoulderProblemWrapper
 {
-    BoulderProblemWrapper( BoulderProblem* ptr ) : pointerToBoulderProblem( ptr ) {}
-    ~BoulderProblemWrapper()
-    {
-        delete pointerToBoulderProblem;
-    }
+    BoulderProblemWrapper( BoulderProblem* ptr );
+    ~BoulderProblemWrapper();
 
     BoulderProblem* pointerToBoulderProblem = nullptr;
 };
 
 struct HoldWrapper
 {
-    HoldWrapper( BoulderProblem::Hold* ptr ) : pointerToHold( ptr ) {}
-    ~HoldWrapper()
-    {
-        delete pointerToHold;
-    }
+    HoldWrapper( BoulderProblem::Hold* ptr );
+    ~HoldWrapper();
 
     BoulderProblem::Hold* pointerToHold = nullptr;
 };
 
 struct TopRopeRouteWrapper
 {
-    TopRopeRouteWrapper( TopRopeRoute* ptr ) : pointerToTopRopeRoute( ptr ) {}
-    ~TopRopeRouteWrapper()
-    {
-        delete pointerToTopRopeRoute;
-    }
+    TopRopeRouteWrapper( TopRopeRoute* ptr );
+    ~TopRopeRouteWrapper();
 
     TopRopeRoute* pointerToTopRopeRoute = nullptr;
 };
 
 struct RouteGradeWrapper
 {
-    RouteGradeWrapper( TopRopeRoute::RouteGrade* ptr ) : pointerToRouteGrade( ptr ) {}
-    ~RouteGradeWrapper()
-    {
-        delete pointerToRouteGrade;
-    }
+    RouteGradeWrapper( TopRopeRoute::RouteGrade* ptr );
+    ~RouteGradeWrapper();
 
     TopRopeRoute::RouteGrade* pointerToRouteGrade = nullptr;
 };
 
 struct MountainWrapper
 {
-    MountainWrapper( Mountain* ptr ) : pointerToMountain( ptr ) {}
-    ~MountainWrapper()
-    {
-        delete pointerToMountain;
-    }
+    MountainWrapper( Mountain* ptr );
+    ~MountainWrapper();
 
     Mountain* pointerToMountain = nullptr;
 };
 
 struct ShoeWrapper
 {
-    ShoeWrapper( Shoe* ptr ) : pointerToShoe( ptr ) {}
-    ~ShoeWrapper()
-    {
-        delete pointerToShoe;
-    }
+    ShoeWrapper( Shoe* ptr );
+    ~ShoeWrapper();
 
     Shoe* pointerToShoe = nullptr;
 };
 
 struct TripPlanWrapper
 {
-    TripPlanWrapper( TripPlan* ptr ) : pointerToTripPlan( ptr ) {}
-    ~TripPlanWrapper()
-    {
-        delete pointerToTripPlan;
-    }
+    TripPlanWrapper( TripPlan* ptr );
+    ~TripPlanWrapper();
 
     TripPlan* pointerToTripPlan = nullptr;
 };
 
 struct ClimbPlanWrapper
 {
-    ClimbPlanWrapper( ClimbPlan* ptr ) : pointerToClimbPlan( ptr ) {}
-    ~ClimbPlanWrapper()
-    {
-        delete pointerToClimbPlan;
-    }
+    ClimbPlanWrapper( ClimbPlan* ptr );
+    ~ClimbPlanWrapper();
 
     ClimbPlan* pointerToClimbPlan = nullptr;
 };
